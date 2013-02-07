5b2ad53eb1e697b5d5e47f9e096e9ec38adb9593
Ben Havilland <ben@havilland.net>
2013-02-06 18:57:37 -0800

    Config change for static files & heroku.

--- settings.py
+++ settings.py
@@ -268,6 +268,14 @@ STATIC_URL = "/static/"
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(STATIC_ROOT, 'static'),
+)
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"

7cfad2a9624e2fc63d8dd0b015ab96bfb4649376
Ben Havilland <ben@havilland.net>
2013-03-28 00:36:29 -0700

    Modified shop to only allow sales within 94044 while we get our commercial kitchen license.

--- mods/shipping_mod.py
+++ mods/shipping_mod.py
@@ -1,29 +1,42 @@
 from mezzanine.conf import settings
 from django.utils.translation import ugettext as _
+from django.http import HttpResponseRedirect
 
 from cartridge.shop.utils import set_shipping
-
+from cartridge.shop.checkout import CheckoutError
 def fudge_shipping_handler(request, order_form):
-
-    # Check for the locals
-    isLocal = False
-    if request.POST['shipping_detail_city'].lower() == 'pacifica' \
-      and (request.POST['shipping_detail_state'].lower() == 'ca' \
-      or request.POST['shipping_detail_state'].lower() == 'california') :
-        isLocal = True
-    elif request.POST['shipping_detail_postcode'] == '94044':
-        isLocal = True
-    if isLocal:
-      set_shipping(request, _("Free shipping for locals"),0.00)
-      return()
-
-    # Check for free shipping theshold
-    if request.cart.total_price() >= settings.FREE_SHIP_VALUE:
-      set_shipping(request, _("Free shipping"),0.00)
-      return()
+    if is_local(request):
+      if request.cart.total_price() >= settings.FREE_SHIP_VALUE:
+        set_shipping(request, _("Free Delivery"),0.00)
+      else:
+        set_shipping(request, _("Pacifica Delivery"),2.00)
     else:
-      spendMore = settings.FREE_SHIP_VALUE - request.cart.total_price()
-    if not request.session.get("free_shipping"):
-        settings.use_editable()
-        set_shipping(request, _("Spend only $"+str(spendMore)+" more and get free shipping. Shipping"),
-                     settings.SHOP_DEFAULT_SHIPPING_VALUE)
+      request.cart.invalid_address = True
+
+def is_local(request):
+  if request.POST['shipping_detail_city'].lower() == 'pacifica' \
+    and (request.POST['shipping_detail_state'].lower() == 'ca' \
+    or request.POST['shipping_detail_state'].lower() == 'california') \
+    and request.POST['shipping_detail_postcode'] == '94044':
+      return True
+  return False
+
+def within_county_limits(request):
+  for location in settings.ZIP_CODE_WHITELIST:
+    if verify_zip(request, location):
+      return True
+    if verify_city(request, location):
+      return True
+  return False
+
+def verify_zip(request, location):
+  for zip_code, city in location.items():
+    if zip_code == request.POST['shipping_detail_postcode']:
+      return True
+  return False
+
+def verify_city(request, location):
+  for zip_code, city in location.items():
+    if city == request.POST['shipping_detail_city']:
+      return True
+  return False
